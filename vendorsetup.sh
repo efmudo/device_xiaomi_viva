c449498e8ea21cb58e61f8b9aa069f76a7a0e379
efmudo <efrizal7725@gmail.com>
2023-10-31 12:28:32 +0000

    viva/clang set proton-clang
    Signed-off-by: efmudo <efrizal7725@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+# Clang
+git clone https://github.com/efmudo/proton-clang prebuilts/clang/host/linux-x86/proton-clang --depth=1
+git clone https://github.com/efmudo/prebuilts_gcc_linux-x86_aarch64_aarch64-linaro-7 prebuilts/clang/host/linux-x86/proton-clang/los-4.9-64 --depth=1
+git clone https://github.com/efmudo/linaro_arm-linux-gnueabihf-7.5 prebuilts/clang/host/linux-x86/proton-clang/los-4.9-32 --depth=1
